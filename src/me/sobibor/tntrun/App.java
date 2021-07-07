36514491ad47ed7ed0421f891c419f56c58df6e8
Sobibor75 <86809532+Sobibor75@users.noreply.github.com>
2021-07-07 17:30:33 +0400

    init

---
+++ src/me/sobibor/tntrun/App.java
@@ -0,0 +1,13 @@
+package me.sobibor.tntrun;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin implements Listener{
+    @Override
+    public void onEnable(){
+        Bukkit.getPluginManager().registerEvents(this,this);
+    }
+
+}
