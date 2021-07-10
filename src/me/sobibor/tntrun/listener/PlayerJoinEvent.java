dffdf161c1b73e5ef4f908647ddf2e17e256f4a2
Sobibor75 <86809532+Sobibor75@users.noreply.github.com>
2021-07-10 19:30:44 +0400

    Сделал немного нового

--- src/me/sobibor/tntrun/listener/PlayerJoinEvent.java
+++ src/me/sobibor/tntrun/listener/PlayerJoinEvent.java
@@ -6,6 +6,9 @@ import me.sobibor.tntrun.player.UserInfo;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerJoinEvent implements Listener {
@@ -25,8 +28,17 @@ public class PlayerJoinEvent implements Listener {
 
         //добавляю игрока на сервера
         app.getLivePlayers().add(e.getPlayer().getUniqueId());
+        // убрал сообщения при входе нна сервер
+        e.setJoinMessage(null);
     }
-
+//убрал урон от падения
+    public void fallDamage(EntityDamageByEntityEvent e) {
+        e.setCancelled(true);
+    }
+//убрал файты
+    public void damageEntity(EntityDamageEvent e) {
+        e.setCancelled(true);
+    }
 
     public void onQuit(org.bukkit.event.player.PlayerQuitEvent e) {
         app.getLivePlayers().remove(e.getPlayer().getUniqueId());
@@ -41,6 +53,12 @@ public class PlayerJoinEvent implements Listener {
             e.getPlayer().setGameMode(GameMode.SPECTATOR);
         }
     }
+
+    @EventHandler
+    public void food(FoodLevelChangeEvent e) {
+        e.setCancelled(true);
+    }
+
 
 }
 
